afc77b667e53543d9bc21c32870d054963760766
PierreThio <thiolierepierre@gmail.com>
2024-01-05 16:43:47 +0100

    Initial commit

---
+++ assets/javascript/table.js
@@ -0,0 +1,9 @@
+function table(){
+    document.querySelector('.dollar').style.display='none'
+    document.querySelector('#tableauSalaire').style.display='block'
+};
+
+function truc(){
+    document.querySelector('.dollar').style.display='block'
+    document.querySelector('#tableauSalaire').style.display='none'
+}
